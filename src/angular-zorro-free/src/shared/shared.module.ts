cb325b145a37fba94abeef3d1ae3e26b0a424479
stan e <yi.hang@live.com>
2018-07-10 16:34:19 +0800

    导入delon的模块

--- src/angular-zorro-free/src/shared/shared.module.ts
+++ src/angular-zorro-free/src/shared/shared.module.ts
@@ -1,4 +1,5 @@
 ﻿import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { AbpModule } from '@abp/abp.module';
 import { RouterModule } from '@angular/router';
@@ -8,10 +9,53 @@ import { AppUrlService } from './nav/app-url.service';
 import { AppAuthService } from './auth/app-auth.service';
 import { AppRouteGuard } from './auth/auth-route-guard';
 
+// delon
+import { AlainThemeModule } from '@delon/theme';
+import { DelonABCModule } from '@delon/abc';
+import { DelonACLModule } from '@delon/acl';
+import { DelonFormModule } from '@delon/form';
+
+// region: third libs
+import { NgZorroAntdModule } from 'ng-zorro-antd';
+import { CountdownModule } from 'ngx-countdown';
+
+
+
+const THIRDMODULES = [
+  NgZorroAntdModule,
+  CountdownModule
+];
+// endregion
+
+
 @NgModule({
-  imports: [CommonModule, AbpModule, RouterModule],
+  imports: [
+    CommonModule,
+    AbpModule,
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule,
+
+    AlainThemeModule.forChild(),
+    DelonABCModule,
+    DelonACLModule,
+    DelonFormModule,
+    // third libs
+    ...THIRDMODULES
+  ],
   declarations: [],
-  exports: [],
+  exports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule,
+    AlainThemeModule,
+    DelonABCModule,
+    DelonACLModule,
+    DelonFormModule,
+    // third libs
+    ...THIRDMODULES,
+  ],
 })
 export class SharedModule {
   static forRoot(): ModuleWithProviders {
