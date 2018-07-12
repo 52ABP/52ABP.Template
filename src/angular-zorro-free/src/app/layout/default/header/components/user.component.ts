2fc66aae238540e50103a1b5a30d2ae90c2ae5df
角落的白板报 <werltm@hotmail.com>
2018-07-12 18:12:20 +0800

    右侧头部菜单栏，右上角增加登出功能。

--- src/angular-zorro-free/src/app/layout/default/header/components/user.component.ts
+++ src/angular-zorro-free/src/app/layout/default/header/components/user.component.ts
@@ -1,34 +1,33 @@
-import { Component, Inject } from '@angular/core';
+import { AppComponentBase } from 'shared/app-component-base';
+import { Component, Injector, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { SettingsService } from '@delon/theme';
 import { DA_SERVICE_TOKEN, ITokenService } from '@delon/auth';
+import { AppAuthService } from '@shared/auth/app-auth.service';
 
 @Component({
   selector: 'header-user',
   template: `
   <nz-dropdown nzPlacement="bottomRight">
     <div class="item d-flex align-items-center px-sm" nz-dropdown>
-      <nz-avatar [nzSrc]="settings.user.avatar" nzSize="small" class="mr-sm"></nz-avatar>
-      {{settings.user.name}}
+      <nz-avatar [nzSrc]="" nzSize="small" class="mr-sm"></nz-avatar>
+
     </div>
     <div nz-menu class="width-sm">
       <div nz-menu-item [nzDisabled]="true"><i class="anticon anticon-user mr-sm"></i>个人中心</div>
       <div nz-menu-item [nzDisabled]="true"><i class="anticon anticon-setting mr-sm"></i>设置</div>
       <li nz-menu-divider></li>
-      <div nz-menu-item (click)="logout()"><i class="anticon anticon-setting mr-sm"></i>退出登录</div>
+      <div nz-menu-item (click)="logout()"><i class="anticon anticon-setting mr-sm"></i> {{l('Logout')}}</div>
     </div>
   </nz-dropdown>
   `,
 })
-export class HeaderUserComponent {
-  constructor(
-    public settings: SettingsService,
-    private router: Router,
-    @Inject(DA_SERVICE_TOKEN) private tokenService: ITokenService,
-  ) {}
-
-  logout() {
-    this.tokenService.clear();
-    this.router.navigateByUrl(this.tokenService.login_url);
+export class HeaderUserComponent extends AppComponentBase implements OnInit {
+  constructor(injector: Injector, private _authService: AppAuthService) {
+    super(injector);
+  }
+  ngOnInit(): void {}
+  logout(): void {
+    this._authService.logout();
   }
 }
--- src/angular-zorro-free/src/app/layout/default/header/header.component.ts
+++ src/angular-zorro-free/src/app/layout/default/header/header.component.ts
@@ -4,15 +4,15 @@ import { SettingsService } from '@delon/theme';
 @Component({
   selector: 'layout-header',
   templateUrl: './header.component.html',
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
 })
 export class HeaderComponent {
   searchToggleStatus: boolean;
 
-  constructor(public settings: SettingsService) { }
+  constructor(public settings: SettingsService) {}
 
   toggleCollapsedSideabar() {
-    let collapsed = !this.settings.layout.collapsed;
+    const collapsed = !this.settings.layout.collapsed;
     this.settings.setLayout('collapsed', collapsed);
     abp.event.trigger('abp.theme-setting.collapsed', collapsed);
   }
