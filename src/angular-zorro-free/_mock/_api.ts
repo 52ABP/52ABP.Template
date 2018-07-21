9663acf1372555596403ad4c2f1897a1be662695
桐铭 梁 <werltm@hotmail.com>
2018-07-21 20:12:49 +0800

    补充控制台内容信息

--- src/angular-zorro-free/_mock/_api.ts
+++ src/angular-zorro-free/_mock/_api.ts
@@ -3,6 +3,7 @@ import { MockRequest } from '@delon/mock';
 // region: mock data
 
 const titles = [
+  '.NET CORE',
   'Alipay',
   'Angular',
   'Ant Design',
@@ -11,9 +12,14 @@ const titles = [
   'React',
   'Vue',
   'Webpack',
+  'NG-ZORRO',
+  'Ng Alain',
+  'ABP',
+  '52ABP',
 ];
 
 const avatars = [
+  'https://docs.microsoft.com/zh-cn/dotnet/images/hub/netcore.svg', // .net core
   'https://gw.alipayobjects.com/zos/rmsportal/WdGqmHpayyMjiEhcKoVE.png', // Alipay
   'https://gw.alipayobjects.com/zos/rmsportal/zOsKZmFRdUtvpqCImOVY.png', // Angular
   'https://gw.alipayobjects.com/zos/rmsportal/dURIMkkrRFpPgTuzkwnB.png', // Ant Design
@@ -22,6 +28,10 @@ const avatars = [
   'https://gw.alipayobjects.com/zos/rmsportal/kZzEzemZyKLKFsojXItE.png', // React
   'https://gw.alipayobjects.com/zos/rmsportal/ComBAopevLwENQdKWiIn.png', // Vue
   'https://gw.alipayobjects.com/zos/rmsportal/nxkuOJlFJuAUhzlMTCEe.png', // Webpack
+  'https://ng.ant.design/assets/img/logo.svg', // NG-ZORRO  9
+  'https://ng-alain.com/assets/img/logo-color.svg', // ng -alain 10
+  'https://aspnetboilerplate.com/images/logos/abp-logo-long.png', // abp
+  'https://avatars2.githubusercontent.com/u/33684174?s=200&v=4', // 52abp  12
 ];
 const covers = [
   'https://gw.alipayobjects.com/zos/rmsportal/HrxcVbrKnCJOZvtzSqjN.png',
@@ -111,60 +121,67 @@ function getNotice(): any[] {
       id: 'xxx1',
       title: titles[0],
       logo: avatars[0],
-      description: '那是一种内在的东西， 他们到达不了，也无法触及的',
+      description:
+        '.NET CORE 是微软新一代的跨平台的后端框架， 是.NET Framework的进化版本,.NET Core的核心点： 创新、开源、跨平台，当前版本为.net core2.x',
       updatedAt: new Date(),
       member: '科学搬砖组',
-      href: '',
-      memberLink: '',
+      href: '微软牛逼',
+      memberLink: '.net core',
     },
     {
-      id: 'xxx2',
-      title: titles[1],
-      logo: avatars[1],
-      description: '希望是一个好东西，也许是最好的，好东西是不会消亡的',
-      updatedAt: new Date('2017-07-24'),
-      member: '全组都是吴彦祖',
-      href: '',
-      memberLink: '',
-    },
-    {
-      id: 'xxx3',
-      title: titles[2],
-      logo: avatars[2],
-      description: '城镇中有那么多的酒馆，她却偏偏走进了我的酒馆',
-      updatedAt: new Date(),
-      member: '中二少女团',
-      href: '',
-      memberLink: '',
-    },
-    {
-      id: 'xxx4',
-      title: titles[3],
-      logo: avatars[3],
-      description: '那时候我只会想自己想要什么，从不想自己拥有什么',
+      id: 'xxx6',
+      title: titles[11],
+      logo: avatars[11],
+      description:
+        'ASP.NET Boilerplate是一个用最佳实践和流行技术开发现代WEB应用程序的新起点，它旨在成为一个通用的WEB应用程序框架和项目模板框架',
       updatedAt: new Date('2017-07-23'),
-      member: '程序员日常',
-      href: '',
-      memberLink: '',
-    },
-    {
-      id: 'xxx5',
-      title: titles[4],
-      logo: avatars[4],
-      description: '凛冬将至',
-      updatedAt: new Date('2017-07-23'),
-      member: '高逼格设计天团',
-      href: '',
+      member: '还在思考中',
+      href: 'ABP框架  土牛威武  ',
       memberLink: '',
     },
     {
       id: 'xxx6',
-      title: titles[5],
-      logo: avatars[5],
-      description: '生命就像一盒巧克力，结果往往出人意料',
+      title: titles[12],
+      logo: avatars[12],
+      description:
+        '52ABP是一个将目前流行的框架进行了整合，核心以ABP框架和ng-zorro为标准进行研发的应用型框架信息。',
+      updatedAt: new Date('2017-07-23'),
+      member: '还在思考中',
+      href: 'ABP框架  土牛威武  ',
+      memberLink: '',
+    },
+    {
+      id: 'xxx2',
+      title: titles[2],
+      logo: avatars[2],
+      description:
+        'Angular 是一个开发平台。它能帮你更轻松的构建 Web 应用。Angular 集声明式模板、依赖注入、端到端工具和一些最佳实践于一身，为你解决开发方面的各种挑战。Angular 为开发者提升构建 Web、手机或桌面应用的能力。',
+      updatedAt: new Date('2017-07-24'),
+      member: '全组都是吴彦祖',
+      href: 'Angular',
+      memberLink: 'Angular',
+    },
+
+    {
+      id: 'xxx4',
+      title: titles[9],
+      logo: avatars[9],
+      description:
+        '这里是 Ant Design 的 Angular 实现，开发和服务于企业级后台产品。也是52ABP框架的前端核心之一    ',
+      updatedAt: new Date('2017-07-23'),
+      member: '设计天团',
+      href: 'NG-ZORRO',
+      memberLink: 'NG-ZORRO',
+    },
+    {
+      id: 'xxx5',
+      title: titles[10],
+      logo: avatars[10],
+      description:
+        '一个基于 Antd的设计，整合了NG-ZORRO的 中后台前端解决方案，提供更多通用性业务模块，让开发者更加专注于业务。',
       updatedAt: new Date('2017-07-23'),
       member: '骗你来学计算机',
-      href: '',
+      href: 'Ng Alain   ',
       memberLink: '',
     },
   ];
