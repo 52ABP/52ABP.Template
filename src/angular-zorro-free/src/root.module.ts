9215cb263df751f2b747a67be8d21eb0e2d8f244
角落的白板报 <werltm@hotmail.com>
2018-08-20 15:02:10 +0800

    删除无用的图片文件。

--- src/angular-zorro-free/src/root.module.ts
+++ src/angular-zorro-free/src/root.module.ts
@@ -34,7 +34,7 @@ export function appInitializerFactory(injector: Injector) {
         );
         appSessionService.init().then(
           result => {
-            abp.ui.clearBusy();
+            //  abp.ui.clearBusy();
             resolve(result);
           },
           err => {
