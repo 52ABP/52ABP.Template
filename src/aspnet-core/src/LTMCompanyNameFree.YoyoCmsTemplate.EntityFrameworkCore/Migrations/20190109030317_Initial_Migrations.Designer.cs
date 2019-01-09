1dda9af5c0ae8b8d9c7c61aa79fcdc93036185de
角落的白板报 <werltm@hotmail.com>
2019-01-09 11:05:08 +0800

    切换默认为 windows访问 切换默认的ngswg

--- src/aspnet-core/src/LTMCompanyNameFree.YoyoCmsTemplate.EntityFrameworkCore/Migrations/20190109030317_Initial_Migrations.Designer.cs
+++ src/aspnet-core/src/LTMCompanyNameFree.YoyoCmsTemplate.EntityFrameworkCore/Migrations/20190109030317_Initial_Migrations.Designer.cs
@@ -10,14 +10,14 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LTMCompanyNameFree.YoyoCmsTemplate.Migrations
 {
     [DbContext(typeof(YoyoCmsTemplateDbContext))]
-    [Migration("20190109025517_Initial_Migrations")]
+    [Migration("20190109030317_Initial_Migrations")]
     partial class Initial_Migrations
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "2.1.1-rtm-30846")
+                .HasAnnotation("ProductVersion", "2.1.4-rtm-31024")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128)
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
--- src/aspnet-core/src/LTMCompanyNameFree.YoyoCmsTemplate.EntityFrameworkCore/Migrations/YoyoCmsTemplateDbContextModelSnapshot.cs
+++ src/aspnet-core/src/LTMCompanyNameFree.YoyoCmsTemplate.EntityFrameworkCore/Migrations/YoyoCmsTemplateDbContextModelSnapshot.cs
@@ -15,7 +15,7 @@ namespace LTMCompanyNameFree.YoyoCmsTemplate.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "2.1.1-rtm-30846")
+                .HasAnnotation("ProductVersion", "2.1.4-rtm-31024")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128)
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
