e4acded9d245c59df81d3979cf84ff0daf3e1993
角落的白板报 <werltm@hotmail.com>
2019-05-08 18:22:30 +0800

    删除无用的前端组件。

--- src/aspnet-core/src/LTMCompanyNameFree.YoyoCmsTemplate.Web.Mvc/Views/Shared/Components/RightSideBar/RightSideBarViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using LTMCompanyNameFree.YoyoCmsTemplate.Configuration.Ui;
-
-namespace LTMCompanyNameFree.YoyoCmsTemplate.Web.Views.Shared.Components.RightSideBar
-{
-    public class RightSideBarViewModel
-    {
-        public UiThemeInfo CurrentTheme { get; set; }
-    }
-}
